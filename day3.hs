de7e417900ad67da6d2304726880e732b0dc123c
Mac15001900 <mac15001900@gmail.com>
2025-12-04 16:34:00 +0100

    Day 3 part 2

--- day3.hs
+++ day3.hs
@@ -1,13 +1,18 @@
 import           Data.List
+import           Data.Maybe ( fromJust )
 
 import           MUtils
 
-joltage :: [Int] -> Int
-joltage [] = 0
-joltage [x] = 0
-joltage (x : xs) = max (x * 10 + maximum xs) (joltage xs)
+joltage :: Int -> [Int] -> Int
+joltage 0 _ = 0
+joltage n xs = best * 10 ^ (n - 1) + joltage (n - 1) (drop (elemIndex best xs |> fromJust |> (+ 1)) xs)
+  where
+    best = maximum (take (length xs - (n - 1)) xs)
 
 part1 :: [String] -> Int
-part1 xs = map2 singleton xs |> map2 readInt |> map joltage |> sum
+part1 xs = map2 singleton xs |> map2 readInt |> map (joltage 2) |> sum
+
+part2 :: [String] -> Int
+part2 xs = map2 singleton xs |> map2 readInt |> map (joltage 12) |> sum
 
 test = [ "987654321111111", "811111111111119", "234234234234278", "818181911112111" ]
